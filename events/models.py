a62e9a54f4b2b843e117ee88c8951bcb43fdb18f
herooww <herogallarte@yahoo.com.ph>
2018-11-05 19:02:47 +0800

    added  events model

---
+++ events/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+
+class Event(models.Model):
+    name = models.CharField("Name of Events", 
+        max_length = 100, default = "")
+    description = models.TextField("Description",
+        max_length = 2000, blank= True)
+    max_slots = models.IntegerField("Max Slots ",
+        blank = True, null= True)
+    date_from =models.DateField("Start date")
+    date_to =models.DateField("End date")
+    time_from=models.TimeField("Start time")
+    time_to=models.TimeField("End time")
+    venue=models.CharField("Venue", max_length= 100)
+    date_created=models.DateTimeField("Date created", auto_now_add= True)
+
+    def __str__(self):
+        return self.name
