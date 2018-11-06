7f5952e6a7fbc527be05d7a2845e5b0eba5f6c4f
herooww <herogallarte@yahoo.com.ph>
2018-11-06 18:24:43 +0800

    implemented event _detail,login,logout&bootstrap

--- events/views.py
+++ events/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.views.generic import TemplateView, ListView
-
+from django.views.generic import TemplateView, ListView, DetailView
 from .models import Event
 
 class HomePageView(TemplateView):
     template_name = "home.html"
 
-
 class EventListView(ListView):
     model = Event
     template_name = "event_list.html"
-    context_object_name = "event list"
+    context_object_name = "event_list"
+
+class EventDetailView(DetailView):
+    model = Event
+    template_name = "event_detail.html"
+    context_object_name = "event"
